64a15ef20f47d8dd2c77be1be2fab258a2db16c6
Астафьев Андрей <a.astafev@bars-open.ru>
2016-09-29 19:17:11 +0300

    изменил структуру

--- dev/ts/components/admin-dashboard.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    templateUrl:'app/html/admin-dashboard.component.html'
-})
-export class AdminDashboardComponent { 
-
-}
--- dev/ts/components/hookah/hookah.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    template:'<h2>hookah</h2>'
-})
-export class HookahComponent { 
-
-}
